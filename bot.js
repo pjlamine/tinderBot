737a891a9edcbb6ab22e4b9a24976e029be5491d
pjlamine <pjlamine@umich.edu>
2017-09-22 23:41:55 -0400

    updated userToken

--- bot.js
+++ bot.js
@@ -7,12 +7,15 @@
 var tinder = require('tinderjs');
 var client = new tinder.TinderClient();
 
+var userToken = "EAABtx7GVqQQBAMQ6bF2SslQZBw9MZBeBlZAentkdnMWoC5Ovv48xJr67LfSROsieiyfmIOGF4gWYaOUAnKy7ynz1orXTcrOZCfeyYDDJeHvLtdwJXsJZBTPpwcV7Yb2YPFwuJZAwgK7ipPe7bsEhKocBM0NV6h9s1CcdZCXGxZCmrwZDZD";
+var userid = "238900436637345";
+
 client.authorize(
-  <fb user token>,
-  <fb user id>,
+  userToken,
+  userid,
   function() {
     client.getRecommendations(10, function(error, data){
       console.log(data.results);
     });
   });
-});
+// });
