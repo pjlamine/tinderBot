be2aef9e87e2a6d2c5171935f7f0c560f4a65f32
Paul Lamine <pjlamine@users.noreply.github.com>
2017-10-05 20:29:17 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,3 @@
-// password: Mikegetspussy
-// email: mike_tinder_mike@outlook.com
-// id: 100015521700862
-// user token: EAABtx7GVqQQBAH7aZCO3MaflK6L2VxYt5mUkKG87ei5RiplXQhctJrvYL9Gx7OCpwwdGRKGwRZBmsuONB0ajKhvwpZACyQsRGJRMyEZB6Bzieb3KdFnllUN4EvKORTml7aFLGO5gj2n3ox0U120NoBgzRaBvv3eRU7jPeVf1eHHF4WnbCFRUC8f9JpSvWNsZD
 
 
 var tinder = require('tinderjs');
